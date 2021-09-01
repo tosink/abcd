8386a88aac140d890ebb66fea75f88dd830a7f31
TK <komolafetosin@gmail.com>
2021-09-01 13:54:38 +0100

    sotkc

--- sale_stock/models/__init__.py
+++ sale_stock/models/__init__.py
@@ -2,9 +2,9 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from . import account_move
-from . import res_company
-from . import sale_order
-from . import res_config_settings
-from . import stock
-from . import product_template
-from . import res_users
+# from . import res_company
+# from . import sale_order
+# from . import res_config_settings
+# from . import stock
+# from . import product_template
+# from . import res_users
