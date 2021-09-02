746a5acdc77aac9fe8935e88bb1ab94c0700588b
TK <komolafetosin@gmail.com>
2021-09-02 11:18:17 +0100

    changes

--- sale_stock/__manifest__.py
+++ sale_stock/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'Sales and Warehouse Management',
-    'version': '1.1',
+    'version': '1.2',
     'category': 'Hidden',
     'summary': 'Quotation, Sales Orders, Delivery & Invoicing Control',
     'description': """
