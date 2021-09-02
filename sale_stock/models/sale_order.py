7b2e207268a51e1416a28d608a5d374c3dec8e96
TK <komolafetosin@gmail.com>
2021-09-02 11:37:33 +0100

    changes

--- sale_stock/models/sale_order.py
+++ sale_stock/models/sale_order.py
@@ -10,3 +10,9 @@ class SaleOrder(models.Model):
     _inherit = "sale.order"
 
     picking_ids = fields.One2many('stock.picking', 'sale_id', string='Transfers')
+
+
+class StockPicking(models.Model):
+    _inherit = 'stock.picking'
+
+    sale_id = fields.Many2one(related="group_id.sale_id", string="Sales Order", store=True, readonly=False)
